320981f8780b913ba39f46ace54f10727402f203
Srijoy Paul <srijoykumar1507@gmail.com>
2024-03-19 12:22:05 +0530

    Initial Http Express Socket server setup, connect  and sample messages

---
+++ index.ts
@@ -0,0 +1,39 @@
+/**
+ * Basically, this is the socket.io server to handle requests from client, (yeah it is different from the normal server that I used to configure yet in each of the prev projects.)
+ * we can see it is integrated on the http server.
+ */
+
+const express=require('express');
+const dotenv = require("dotenv").config({ path: '../.env' });
+const {Server}=require('socket.io');
+const {createServer}=require('http');
+const cors=require('cors');
+
+const PORT=process.env.PORT;
+
+const app=express();
+app.use(cors());
+
+const server=createServer(app);
+const io=new Server(server,{ /** here we are doing the integration, basically making our http express server a socket server. And actually running the socket server, so it can listen to any requests */
+    cors:{
+        origin:'*' //this is to allow the client running on different port access the socket server
+    }
+});
+
+/**
+ * Basically 'connection' is a predefined event of socket.io
+ */
+io.on('connection',(socket)=>{ //this connection gets triggered whenever a client gets connected to the socket server.(this event is used for server side)
+    // socket.on('chat message',(mssg)=>{
+    //     console.log('message:' + mssg);
+    // });
+    console.log("hi there, i am server there");
+    
+    
+});
+
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port number ${PORT}`);
+})
